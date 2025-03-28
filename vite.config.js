24f93a0aacdcc46f966c4a2e8269388da07b7fc2
Nitin919 <nitinshar999@gmail.com>
2025-03-28 21:46:17 +0000

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
   build: {
     outDir: 'dist',
-  },
+    rollupOptions: {
+      external: [],
+    }
+  }
 })
